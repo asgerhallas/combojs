5ffcc0978c1aeaf90a9bd5e060d0ac9d41848bb3
Mads <mwf@energysystems.dk>
2015-01-22 00:08:32 +0100

    Revert "added wrapper to nightwatch runner such that the exit status is always 0,"
    This reverts commit 756017b313b31626ed784f2194bbc31caa7b7271.

--- nightwatch_wrapper.js
+++
@@ -1,8 +0,0 @@
-require('shelljs/global');
-
-// var user = execSync('echo $USER');
-// console.log(process.argv.slice(2));
-exec("nightwatch " + process.argv.slice(2).join(" "));
-// force nightwatch to exit with success to avoid 
-// breaking executing of chained test runs
-process.exit(0) 
